68343ae27aaf34545c93d837a6f82c1c70332872
Thiruparan <thirupparan1994@gmail.com>
2019-01-03 18:10:42 +0530

    added file

---
+++ src/main/java/com/sgic/SpringBootWebScocket/SpringBootWebScocketApplication.java
@@ -0,0 +1,14 @@
+package com.sgic.SpringBootWebScocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebScocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebScocketApplication.class, args);
+	}
+
+}
+
